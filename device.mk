2f32636577ba44d4bbe6b1e6fb6183fb3da38bbf
Evilmove <evilmove4378@gmail.com>
2025-02-10 01:02:26 +0530

    adding opluscamera

--- device.mk
+++ device.mk
@@ -490,3 +490,7 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/realme/rmx3371/rmx3371-vendor.mk)
+
+# OplusCamera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
