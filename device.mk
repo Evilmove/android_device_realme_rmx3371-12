b90315b28c3c67106363a1f7e7e756719e577c86
Evilmove <evilmove4378@gmail.com>
2025-02-10 20:45:23 +0530

    rmx3371:adding gapps

--- device.mk
+++ device.mk
@@ -187,6 +187,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+#Gapps import form vendor/gapps
+-include vendor/gapps/arm64/arm64-vendor.mk
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
